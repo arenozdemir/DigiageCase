e3d3ed2bba78d1919309febbe73b2e9f5a1d4a40
Yavuz Kopar <kopar.yavuz@gmail.com>
2022-12-31 00:34:06 +0300

    v30

--- DigiageCase/Assets/Script/PlayerScript.cs
+++ DigiageCase/Assets/Script/PlayerScript.cs
@@ -16,6 +16,7 @@ public class PlayerScript : MonoBehaviour
     }
     private void Update()
     {
+        transform.position += Vector3.back * Time.deltaTime * 5;
         MovementHandler();
     }
     private void OnTriggerEnter(Collider other)
@@ -51,11 +52,12 @@ public class PlayerScript : MonoBehaviour
     Vector3 mousePosition;
     private void MovementHandler()
     {
-        mousePosition = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x,Input.mousePosition.y, Camera.main.transform.position.z - 9));
+        float z = Camera.main.transform.position.z - transform.position.z ;
+        mousePosition = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x,Input.mousePosition.y, z));
        // mousePosition.z = Camera.main.transform.position.z - 9;
         mousePosition.y = 0;
          transform.position = new Vector3(Mathf.Clamp(transform.position.x,-5,5),transform.position.y,transform.position.z);
-        transform.position += Vector3.back * Time.deltaTime*5;
+        
         
         if (Input.GetMouseButtonDown(0))
         {
@@ -66,7 +68,8 @@ public class PlayerScript : MonoBehaviour
 
         if (Input.GetMouseButton(0))
         {
-               transform.position = Vector3.Lerp(transform.position, mousePosition + offset, Time.deltaTime);
+               transform.position = Vector3.Lerp(transform.position, mousePosition + offset, Time.deltaTime * 2);
+         //     transform.position = transform.position + offset * Time.deltaTime;
            // transform.position = mousePosition + offset;
         }
     }
